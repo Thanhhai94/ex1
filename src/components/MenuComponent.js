053cffbe7c88fa51472c674139ac973499c37be1
hai1994 <haivtfx14871@funix.edu.vn>
2022-02-13 23:04:38 +0700

    upEx1

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -6,7 +6,8 @@ class Menu extends React.Component {
         super(props);
         this.state = {
            selectedDish: null
-        };
+        }
+        console.log("constructor")
     }
 
     onDishSelect(dish) {
@@ -14,16 +15,68 @@ class Menu extends React.Component {
         selectedDish: dish
       })
     }
+
+    componentDidMount(){
+      console.log("did mount")
+    }
+    componentWillUnmount(){
+      console.log("unmount")
+    }
+    componentDidUpdate(){
+      console.log('didupdate')
+    }
+
+    renderList(data){
+      return data.comments.map(el => 
+        <div key={el.id}>
+        <p>{el.comment}</p>
+        <p>--{el.author} , {Date(el.date)}</p>
+        </div>
+        
+        )}
+
+    renderComment(dish){
+      
+      
+     
+      if (dish != null)
+          return(
+            
+            <div className="col-12 col-md-5 m-1">
+              <Card>
+                 
+                  <CardBody>
+                    <CardTitle style={{fontWeight: "600"}}>Comment</CardTitle>
+                    <CardText>{
+                      dish && this.renderList(dish)
+                      }</CardText>
+                  </CardBody>
+              </Card>
+            </div>
+            
+          );
+      else
+          return(
+              <div></div>
+          );
+    }
+    
+
+
     renderDish(dish) {
       if (dish != null)
           return(
+            
+            <div className="col-12 col-md-5 m-1">
               <Card>
                   <CardImg top src={dish.image} alt={dish.name} />
                   <CardBody>
-                    <CardTitle>{dish.name}</CardTitle>
+                    <CardTitle style={{fontWeight: "600"}}>{dish.name}</CardTitle>
                     <CardText>{dish.description}</CardText>
                   </CardBody>
               </Card>
+            </div>
+            
           );
       else
           return(
@@ -32,6 +85,7 @@ class Menu extends React.Component {
   }
 
     render() {
+      console.log("render")
         const menu = this.props.dishes.map((dish) => {
             return (
               <div key={dish.id} className="col-12 col-md-5 m-1">
@@ -52,8 +106,10 @@ class Menu extends React.Component {
             <div className="row">
                   {menu}
             </div>
-            <div className='row'>
+            <div class="row">
                 {this.renderDish(this.state.selectedDish)}
+                {this.renderComment(this.state.selectedDish)}
+                
             </div>
           </div>
         );
